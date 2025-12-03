8c71a1270451a3707d2ab4ec1f9eb1c39bd80493
Скородумов Сергей <pprofisional41@gmail.com>
2025-12-03 16:03:37 +0300

    Создание первой ветки!

--- Тесты.py
+++ Тесты.py
@@ -1,1 +1,18 @@
+def is_balanced_recursive(s):
+    brackets = {'(': ')', '{': '}', '[': ']'}
 
+    def remove_pairs(s):
+        if not s:
+            return True
+
+
+        for i in range(len(s) - 1):
+            if s[i] in brackets and brackets[s[i]] == s[i + 1]:
+                new_s = s[:i] + s[i + 2:]
+                return remove_pairs(new_s)
+
+        return s == ""
+
+    return remove_pairs(s)
+
+print(is_balanced_recursive("({})"))
