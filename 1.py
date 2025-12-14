a5d9d04cee605c210fee948cdc0edcb5c7ed6b2d
Скородумов Сергей <pprofisional41@gmail.com>
2025-12-14 12:09:55 +0300

    [REF]:[Point] Домашнее задание №14

---
+++ 1.py
@@ -0,0 +1,50 @@
+from Point import Point
+
+p1 = Point(6,9)
+p2 = Point(4,7)
+
+print("Точка A", p1)
+print("Точка Б", p2)
+
+с = p1 + p2
+print("a + b =",с)
+
+с = p1 - p2
+print("a - b =",с)
+
+с = p1 * p2
+print("a * b =",с)
+
+с = p1 / p2
+print("a / b =",с)
+
+с = p1 // p2
+print("a // b =",с)
+
+с = p1 % p2
+print("a % b =",с)
+
+с = p1 == p2
+print("a == b =",с)
+
+с = p1 != p2
+print("a != b =",с)
+
+с = p1 < p2
+print("a < b =",с)
+
+с = p1 <= p2
+print("a <= b =",с)
+
+с = p1 > p2
+print("a > b =",с)
+
+с = p1 >= p2
+print("a >= b =",с)
+
+
+
+
+
+
+
